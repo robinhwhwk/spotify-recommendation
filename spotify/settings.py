db749b5fc825dd8972627a692f7a687d4b7a064b
robinhwhwk <88211687+robinhwhwk@users.noreply.github.com>
2022-12-23 02:34:05 -0500

    prep for deployment

--- spotify/settings.py
+++ spotify/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # Your secret key
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 

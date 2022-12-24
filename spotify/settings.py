0f57778b716d60130a9bc3ce6b7048cb15704dd9
robinhwhwk <88211687+robinhwhwk@users.noreply.github.com>
2022-12-24 02:25:21 -0500

    Update settings.py

--- spotify/settings.py
+++ spotify/settings.py
@@ -128,7 +128,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-CSRF_TRUSTED_ORIGINS = ['https://web-production-c03d.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://web-production-c03d.up.railway.app', 'https://jpopify.com']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
